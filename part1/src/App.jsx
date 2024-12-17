f1f5e688558966dc18093b2db3f5f5e0358943e9
ZomDev3343 <contact.zomzd@gmail.com>
2024-12-17 20:42:23 +0100

    Exercise 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,24 +5,26 @@ import Total from './Total.jsx'
 
 function App() {
 	const course = 'Half Stack application development'
-	const part1 = {
-		name: 'Fundamentals of React',
-		exercises: 10
-	  }
-	  const part2 = {
-		name: 'Using props to pass data',
-		exercises: 7
-	  }
-	  const part3 = {
-		name: 'State of a component',
-		exercises: 14
-	  }
+	const parts = [
+		{
+			name: 'Fundamentals of React',
+			exercises: 10
+		},
+		{
+			name: 'Using props to pass data',
+			exercises: 7
+		},
+		{
+			name: 'State of a component',
+			exercises: 14
+		}
+	]
 
 	return (
 		<div>
-			<Header course={course}/>
-			<Content parts={[part1, part2, part3]}/>
-			<Total exercices={[part1.exercises, part2.exercises, part3.exercises]}/>
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total exercices={[parts[0].exercises, parts[1].exercises, parts[2].exercises]} />
 		</div>
 	)
 }
