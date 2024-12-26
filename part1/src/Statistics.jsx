89cc5fe4f463b03e7ebcfefd0abc64bfc99f619a
ZomDev3343 <contact.zomzd@gmail.com>
2024-12-26 19:22:12 +0100

    exercise 1.10

--- part1/src/Statistics.jsx
+++ part1/src/Statistics.jsx
@@ -1,15 +1,23 @@
+const StatisticsLine = (props) => {
+	return (
+		<>
+			<p>{props.title} {props.value}</p>
+		</>
+	)
+}
+
 const Statistics = (props) => {
 
 	if (props.total > 0)
 		return (
 			<>
 				<h1>Statistics</h1>
-				<p>Good {props.good}</p>
-				<p>Neutral {props.neutral}</p>
-				<p>Bad {props.bad}</p>
-				<p>Total {props.total}</p>
-				<p>Average {props.avg}</p>
-				<p>Positive ratio {props.positiveRatio}</p>
+				<StatisticsLine title="Good" value={props.good}/>
+				<StatisticsLine title="Neutral" value={props.neutral}/>
+				<StatisticsLine title="Bad" value={props.bad}/>
+				<StatisticsLine title="Total" value={props.total}/>
+				<StatisticsLine title="Average" value={props.avg}/>
+				<StatisticsLine title="Positive Ratio" value={props.positiveRatio}/>
 			</>
 		)
 	else
