50de4eb93687d8e76054fee9c9a45b0c3593c5da
ZomDev3343 <contact.zomzd@gmail.com>
2024-12-26 19:17:50 +0100

    exercise 1.9

--- part1/src/Statistics.jsx
+++ part1/src/Statistics.jsx
@@ -1,16 +1,24 @@
 const Statistics = (props) => {
 
-	return (
-		<>
-			<h1>Statistics</h1>
-			<p>Good {props.good}</p>
-			<p>Neutral {props.neutral}</p>
-			<p>Bad {props.bad}</p>
-			<p>Total {props.total}</p>
-			<p>Average {props.avg}</p>
-			<p>Positive ratio {props.positiveRatio}</p>
-		</>
-	)
+	if (props.total > 0)
+		return (
+			<>
+				<h1>Statistics</h1>
+				<p>Good {props.good}</p>
+				<p>Neutral {props.neutral}</p>
+				<p>Bad {props.bad}</p>
+				<p>Total {props.total}</p>
+				<p>Average {props.avg}</p>
+				<p>Positive ratio {props.positiveRatio}</p>
+			</>
+		)
+	else
+		return (
+			<>
+				<h1>Statistics</h1>
+				<p>No feedback given</p>
+			</>
+		)
 }
 
 export default Statistics
