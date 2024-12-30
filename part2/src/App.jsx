a2074dc586e2fc32d82459d00c2d6dd7359320f3
ZomDev3343 <contact.zomzd@gmail.com>
2024-12-30 18:02:16 +0100

    exercise 2.6

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,68 +1,39 @@
-import Course from "./Course"
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
-	const courses = [
-		{
-			name: 'Half Stack application development',
-			id: 1,
-			parts: [
-				{
-					name: 'Fundamentals of React',
-					exercises: 10,
-					id: 1
-				},
-				{
-					name: 'Using props to pass data',
-					exercises: 7,
-					id: 2
-				},
-				{
-					name: 'State of a component',
-					exercises: 14,
-					id: 3
-				},
-				{
-					name: 'Test',
-					exercises: 89,
-					id: 4
-				}
-			]
-		},
-		{
-			name: 'Test course',
-			id: 2,
-			parts: [
-				{
-					name: 'Fundamentals of test',
-					exercises: 10,
-					id: 1
-				},
-				{
-					name: 'Using test to pass data',
-					exercises: 24,
-					id: 2
-				},
-				{
-					name: 'Test of a component',
-					exercises: 2,
-					id: 3
-				},
-				{
-					name: 'Another Test',
-					exercises: 13,
-					id: 4
-				}
-			]
-		}
-	]
-	
-	return (
-		<div>
-			<h1>Web development curriculum</h1>
-			{courses.map(course => <Course key={course.id} course={course} />)}
-		</div>
-	)
+  const onNameChange = (ev) => {
+	setNewName(ev.target.value)
+	console.log("New name value", newName)
+  }
+
+  const onNameAdded = (ev) => {
+	ev.preventDefault()
+	setPersons(persons.concat({name: newName}))
+	setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onNameAdded}>
+        <div>
+          name: <input value={newName} onChange={onNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+	  <ul>
+	      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+	  </ul>
+    </div>
+  )
 }
 
 export default App
--- part2/src/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({course}) => {
-	return (
-		<div>
-			<strong>total of {course.parts.map(part => part.exercises).reduce((a, b) => a + b, 0)} exercises</strong>
-		</div>
-	)
-}
-
-export default Total
